a94e7d64007b12f24c62ca75535bde1e7a8a9b13
Matias <matiasdelpup1232@gmail.com>
2025-05-31 00:01:55 -0300

    Proyecto de un cajero en Java usando Polimorfismos.

---
+++ Cajero/src/classes/Ejecutable.java
@@ -0,0 +1,14 @@
+//@Author Matias Del Pup
+
+package classes;
+
+public class Ejecutable 
+{
+	public static void main(String args[]) 
+	{
+		ClaseP mensajero = new ClaseHijaConsulta();
+		mensajero.setPlata(500);
+		mensajero.Operaciones();
+	}
+
+}
